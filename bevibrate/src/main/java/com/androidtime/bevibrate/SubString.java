a32f92dad54da467db2ef2c9326021515b46d0f0
Nazmul Masum <nazmulhasan@augmedix.com>
2018-02-01 15:48:39 +0600

    first commit

---
+++ bevibrate/src/main/java/com/androidtime/bevibrate/SubString.java
@@ -0,0 +1,11 @@
+package com.androidtime.bevibrate;
+
+/**
+ * Created by nazmul on 2/1/18.
+ */
+
+public class SubString {
+    public static String subString(String string) {
+        return string.substring(1);
+    }
+}
