9eca89b06a033db61a8126eb71ecf7fcaf1556ca
Nazmul Masum <nazmulhasan@augmedix.com>
2018-02-01 16:28:07 +0600

    Add vibration

---
+++ bevibrate/src/main/java/com/androidtime/bevibrate/Vibration.java
@@ -0,0 +1,29 @@
+package com.androidtime.bevibrate;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+
+/**
+ * Created by nazmul on 2/1/18.
+ */
+
+public class Vibration {
+
+    public static void makeShortTimeVibrate(final int duration, final Context context) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    VibrationEffect.createOneShot(1000, 1);
+                } else {
+                    Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+                    v.vibrate(duration);
+                }
+
+            }
+        }).start();
+
+    }
+}
